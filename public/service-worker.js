d71a50071593098e03be6060c89bbcca802b51b1
AustinJoo97 <austinjoo97@gmail.com>
2021-06-27 19:43:11 -0400

    removed unnecessary comments

--- public/service-worker.js
+++ public/service-worker.js
@@ -21,14 +21,12 @@ self.addEventListener("install", event => {
   );
 });
 
-// The activate handler takes care of cleaning up old caches.
 self.addEventListener("activate", event => {
   const currentCaches = [CACHE_NAME, DATA_CACHE_NAME];
   event.waitUntil(
     caches
       .keys()
       .then(cacheNames => {
-        // return array of cache names that are old to delete
         return cacheNames.filter(
           cacheName => !currentCaches.includes(cacheName)
         );
@@ -45,7 +43,6 @@ self.addEventListener("activate", event => {
 });
 
 self.addEventListener("fetch", event => {
-  // non GET requests are not cached and requests to other origins are not cached
   if (
     event.request.method !== "GET" ||
     !event.request.url.startsWith(self.location.origin)
@@ -54,9 +51,7 @@ self.addEventListener("fetch", event => {
     return;
   }
 
-  // handle runtime GET requests for data from /api routes
   if (event.request.url.includes("/api/images")) {
-    // make network request and fallback to cache if network request fails (offline)
     event.respondWith(
       caches.open(DATA_CACHE_NAME).then(cache => {
         return fetch(event.request)
@@ -70,14 +65,12 @@ self.addEventListener("fetch", event => {
     return;
   }
 
-  // use cache first for all other requests for performance
   event.respondWith(
     caches.match(event.request).then(cachedResponse => {
       if (cachedResponse) {
         return cachedResponse;
       }
 
-      // request is not in cache. make network request and cache the response
       return caches.open(DATA_CACHE_NAME).then(cache => {
         return fetch(event.request).then(response => {
           return cache.put(event.request, response.clone()).then(() => {
